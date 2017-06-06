c4e79442c726a847b5bc08e932ca0730f915e92d
Rishabh Jain <rj1997@users.noreply.github.com>
2017-06-06 23:07:23 +0530

    Delete QuoraBot.py

--- QuoraBot.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ["HTTPS_PROXY"] = "http://ipg_2015003:abhi%4098@192.168.1.107:3128"
-from selenium import webdriver
-path="C:\\Users\\Rishabh Jain\\Downloads\\chromedriver_win32\\chromedriver.exe"
-browser=webdriver.Chrome(path)
-url='https://www.quora.com/Why-is-Facebook-successful'
-browser.get(url)
-
